dda513cb68205183408913e6a61ec7c8d59f3f4c
Wesley Coakley <w@wesleycoakley.com>
2022-12-16 07:58:44 -0500

    Bump to Perl 5.36

--- default.nix
+++ default.nix
@@ -1,13 +1,13 @@
 with import <nixpkgs>{};
-let TextVimColor = perl534Packages.buildPerlPackage {
+let TextVimColor = perl536Packages.buildPerlPackage {
     pname = "Text-VimColor";
     version = "0.29";
     src = fetchurl {
       url = "mirror://cpan/authors/id/R/RW/RWSTAUNER/Text-VimColor-0.29.tar.gz";
       sha256 = "e20d3202c888af3d082a2245db5e87ee774e96fcf6708a30530f2eeb1a90988e";
     };
-    buildInputs = [ vim perl534Packages.FileShareDirInstall perl534Packages.TestFileShareDir ];
-    propagatedBuildInputs = [ perl534Packages.FileShareDir perl534Packages.PathClass ];
+    buildInputs = [ vim perl536Packages.FileShareDirInstall perl536Packages.TestFileShareDir ];
+    propagatedBuildInputs = [ perl536Packages.FileShareDir perl536Packages.PathClass ];
     meta = {
       homepage = "https://github.com/rwstauner/Text-VimColor";
       description = "Syntax highlight text using Vim";
@@ -29,11 +29,11 @@ in pkgs.mkShell rec {
     git-lfs
     discount
 
-    perl
-    perl534Packages.syntax
-    perl534Packages.PathClass
-    perl534Packages.FileShare
-    perl534Packages.SortNaturally
+    perl536
+    perl536Packages.syntax
+    perl536Packages.PathClass
+    perl536Packages.FileShare
+    perl536Packages.SortNaturally
     TextVimColor
   ];
 }
