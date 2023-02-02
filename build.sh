b794409bec300bf1c7d5ee79cbd0cd95255c0adb
Wesley Coakley <w@wesl.ee>
2023-02-02 04:47:27 -0500

    Tag more stuff, flatten the dir hierarchy

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-ikiwiki --setup ./ikiwiki.setup
-
-# HACK to get around autoindex fucking up the tag cloud
-mv ./www/Discover_Cloud/index.html ./www/Discover/
-sed -i 's/Discover Cloud/Discover/g' ./www/Discover/index.html
