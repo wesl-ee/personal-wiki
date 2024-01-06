a31ac2d73bbedda02d9cf1fb23f9d906ff341dbc
Wesley Coakley <w@wesl.ee>
2024-01-06 15:47:57 -0500

    perl bump to 536

--- default.nix
+++ default.nix
@@ -1,13 +1,13 @@
 with import <nixpkgs>{};
-let TextVimColor = perl536Packages.buildPerlPackage {
+let TextVimColor = perl538Packages.buildPerlPackage {
     pname = "Text-VimColor";
     version = "0.29";
     src = fetchurl {
       url = "mirror://cpan/authors/id/R/RW/RWSTAUNER/Text-VimColor-0.29.tar.gz";
       sha256 = "e20d3202c888af3d082a2245db5e87ee774e96fcf6708a30530f2eeb1a90988e";
     };
-    buildInputs = [ vim perl536Packages.FileShareDirInstall perl536Packages.TestFileShareDir ];
-    propagatedBuildInputs = [ perl536Packages.FileShareDir perl536Packages.PathClass ];
+    buildInputs = [ vim perl538Packages.FileShareDirInstall perl538Packages.TestFileShareDir ];
+    propagatedBuildInputs = [ perl538Packages.FileShareDir perl538Packages.PathClass ];
     meta = {
       homepage = "https://github.com/rwstauner/Text-VimColor";
       description = "Syntax highlight text using Vim";
@@ -34,12 +34,13 @@ in pkgs.stdenv.mkDerivation rec {
 
     discount
 
-    perl536
-    perl536Packages.syntax
-    perl536Packages.PathClass
-    perl536Packages.FileShare
-    perl536Packages.TextMultiMarkdown
-    perl536Packages.SortNaturally
+    perl538
+    perl538Packages.syntax
+    perl538Packages.PathClass
+    perl538Packages.FileShare
+    perl538Packages.FileShareDir
+    perl538Packages.TextMultiMarkdown
+    perl538Packages.SortNaturally
     TextVimColor
   ];
 
